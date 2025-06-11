1adf4a9fb8c293e17b655e0ba6ef50e633c01c1b
Dråfølin <derg@drafolin.ch>
2025-06-11 19:28:09 +0200

    Implemented discord RPC with pypresence

--- src/lib/discord_rpc.py
+++ src/lib/discord_rpc.py
@@ -1,22 +1,23 @@
 from tidalapi.media import Track
 import time
 try:
-    import discordrpc
+    import pypresence
 except ImportError:
     print("discordrpc not found, skipping")
-    discordrpc = None
+    pypresence = None
 
-if discordrpc:
-    rpc = discordrpc.RPC(app_id=1379096506065223680)
+if pypresence:
+    rpc = pypresence.Presence(client_id=1379096506065223680)
+    rpc.connect()
 
-def set_activity(track: Track = None, offset_ms: int = 0):
-    if not discordrpc:
-        print("[discordrpc] library not installed, skipping")
+def set_activity(track: Track|None = None, offset_ms: int = 0):
+    if not pypresence:
+        print("[pypresence] library not installed, skipping")
         return
 
     if track is None:
-        rpc.set_activity(
-            act_type=2,
+        rpc.update(
+            activity_type=pypresence.ActivityType.LISTENING,
             details="High Tide",
             state="TIDAL gnome client",
             large_image="hightide_x1024",
@@ -29,19 +30,23 @@ def set_activity(track: Track = None, offset_ms: int = 0):
             ]
         )
     else:
-        rpc.set_activity(
-            act_type=2,
+        rpc.update(
+            activity_type=pypresence.ActivityType.LISTENING,
             details=track.name,
             state=f"By {track.artist.name}",
             large_image=track.album.image() if track.album else "hightide_x1024",
             large_text=track.album.name if track.album else "High Tide",
             small_image="hightide_x1024" if track.album else None,
             small_text="High Tide" if track.album else None,
-            ts_start=int(time.time() * 1_000 - offset_ms),
-            buttons=discordrpc.Button(
-                    "Listen to this song",
-                    track.share_url,
-                    "Get High Tide",
-                    "https://github.com/nokse22/high-tide"
-                ),
+            start=int(time.time() * 1_000 - offset_ms),
+            buttons=[
+                {
+                    "label": "Listen to this song",
+                    "url": f"{track.share_url}?u"
+                },
+                {
+                    "label": "Get High Tide",
+                    "url": "https://github.com/nokse22/high-tide"
+                }
+            ],
         )
