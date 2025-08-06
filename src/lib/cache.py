24749e4c7c6b9ea1d20b86f626a213de74072908
Nokse <nokse@posteo.com>
2025-08-06 17:30:52 +0200

    Add caching (Fix #101)

---
+++ src/lib/cache.py
@@ -0,0 +1,70 @@
+# cache.py
+#
+# Copyright 2025 Nokse <nokse@posteo.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from tidalapi.artist import Artist
+from tidalapi.album import Album
+from tidalapi.media import Track
+from tidalapi.playlist import Playlist
+from tidalapi.mix import Mix
+
+
+class HTCache:
+    artists = {}
+    albums = {}
+    tracks = {}
+    playlists = {}
+    mixes = {}
+
+    def __init__(self, session):
+        self.session = session
+
+    def get_artist(self, artist_id):
+        if artist_id in self.artists:
+            return self.artists[artist_id]
+        artist = Artist(self.session, artist_id)
+        self.artists[artist_id] = artist
+        return artist
+
+    def get_album(self, album_id):
+        if album_id in self.albums:
+            return self.albums[album_id]
+        album = Album(self.session, album_id)
+        self.albums[album_id] = album
+        return album
+
+    def get_track(self, track_id):
+        if track_id in self.tracks:
+            return self.tracks[track_id]
+        track = Track(self.session, track_id)
+        self.tracks[track_id] = track
+        return track
+
+    def get_playlist(self, playlist_id):
+        if playlist_id in self.playlists:
+            return self.playlists[playlist_id]
+        playlist = Playlist(self.session, playlist_id)
+        self.playlists[playlist_id] = playlist
+        return playlist
+
+    def get_mix(self, mix_id):
+        if mix_id in self.mixes:
+            return self.mixes[mix_id]
+        mix = Mix(self.session, mix_id)
+        self.mixes[mix_id] = mix
+        return mix
--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,3 +1,22 @@
+# utils.py
+#
+# Copyright 2025 Nokse <nokse@posteo.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from gi.repository import Gdk, Adw
 from gi.repository import GLib
 from gi.repository import Gio
@@ -49,6 +68,32 @@ def init():
     global navigation_view
     global player_object
     global toast_overlay
+    global cache
+
+
+def get_artist(artist_id):
+    global cache
+    return cache.get_artist(artist_id)
+
+
+def get_album(album_id):
+    global cache
+    return cache.get_album(album_id)
+
+
+def get_track(track_id):
+    global cache
+    return cache.get_track(track_id)
+
+
+def get_playlist(playlist_id):
+    global cache
+    return cache.get_playlist(playlist_id)
+
+
+def get_mix(mix_id):
+    global cache
+    return cache.get_mix(mix_id)
 
 
 def get_favourites():
